b83799f52953ece33065bdc170b513533d5bfeb5
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-04-23 21:50:15 -0500

    CLI stuff

--- src/main/java/io/github/gaming32/squidbeatz2/Main.java
+++ src/main/java/io/github/gaming32/squidbeatz2/Main.java
@@ -11,18 +11,37 @@ import io.github.gaming32.szslib.yaz0.Yaz0InputStream;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Locale;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        final Path romfsPath = Path.of("C:\\Users\\josia\\AppData\\Roaming\\yuzu\\dump\\0100F8F0000A2000\\romfs");
+    public static void main(String[] args) throws Exception {
+        final Path yuzuDir = System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("win")
+            ? Path.of(System.getenv("APPDATA"), "yuzu")
+            : Path.of(System.getProperty("user.home"), ".local/share/yuzu");
+        if (!Files.exists(yuzuDir)) {
+            System.err.println("Yuzu directory (" + yuzuDir + ") not found");
+            System.exit(1);
+        }
+        final Path romfsPath = yuzuDir.resolve("dump/0100F8F0000A2000/romfs");
+        if (!Files.exists(romfsPath)) {
+            System.err.println("Splatoon 2 dump not found. Did you forget to dump Splatoon 2?");
+            System.exit(1);
+        }
+        final Path octoExpansionPath = yuzuDir.resolve("dump/0100F8F0000A3065/romfs");
+        final boolean octoExpansionDumped = Files.exists(octoExpansionPath);
+
         final Map<String, String> songs = getSongIds(romfsPath);
         final Int2ObjectMap<String> songNumberIds = new Int2ObjectOpenHashMap<>(songs.size());
         songs.entrySet()
@@ -30,8 +49,10 @@ public class Main {
             .sorted(Map.Entry.comparingByValue())
             .forEach(entry -> {
                 final int id = songNumberIds.size() + 1;
-                System.out.printf("%2s -- %s\n", id, entry.getValue());
                 songNumberIds.put(id, entry.getKey());
+                if (octoExpansionDumped || Files.exists(romfsPath.resolve(entry.getValue()))) {
+                    System.out.printf("%2s -- %s\n", id, entry.getValue());
+                }
             });
         System.out.print("Enter a song ID: ");
 
@@ -49,17 +70,27 @@ public class Main {
         if (songId == null) {
             System.out.println("Unknown song ID: " + songNumber);
             System.exit(1);
-            return;
         }
 
-        final Path songFile = romfsPath.resolve(Constants.STREAM_PATH).resolve(songId + ".bfstm");
+        final Path relSong = Path.of(Constants.STREAM_PATH, songId + ".bfstm");
+        Path songFile = romfsPath.resolve(relSong);
+        if (!Files.exists(songFile)) {
+            songFile = octoExpansionPath.resolve(relSong);
+        }
         final AudioData data;
         try (SeekableChannelInputStream is = new SeekableChannelInputStream(Files.newByteChannel(songFile))) {
             data = new BCFstmReader().read(is);
         }
-        try (OutputStream os = Files.newOutputStream(Path.of("song.wav"))) {
-            new WaveWriter().writeToStream(data, os);
-        }
+        final byte[] songAudio = new WaveWriter().getFile(data);
+
+        final AudioInputStream ais = AudioSystem.getAudioInputStream(new ByteArrayInputStream(songAudio));
+        final Clip clip = AudioSystem.getClip();
+        clip.open(ais);
+        ((FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN)).setValue(20f * (float)Math.log10(0.2));
+        clip.start();
+        do {
+            Thread.sleep(1000);
+        } while (clip.isRunning());
     }
 
     public static Map<String, String> getSongIds(Path romfsPath) throws IOException {
